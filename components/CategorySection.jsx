899026503cfd30ea4e94139f17bd7b687cc6e8b2
shiv_kumar <shiva90154@gmail.com>
2025-12-01 11:29:05 +0530

    cnages

--- components/CategorySection.jsx
+++ components/CategorySection.jsx
@@ -20,6 +20,7 @@ const CategorySection = ({ category, setCategory }) => {
     router.push(`/packages?category=${value}`);
   };
 
+  
   return (
     <section className="py-16 bg-gray-50/50">
       <div className="max-w-7xl mx-auto px-4">
